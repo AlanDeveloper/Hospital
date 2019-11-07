5a11b625d776f3b63d9b399b9b87cdebbefcf80f
Alan Santos <alanssantos32@gmail.com>
2019-11-07 01:22:58 -0300

    Hora

--- resources/views/template/home.blade.php
+++ resources/views/template/home.blade.php
@@ -7,11 +7,16 @@
 </div>
 
 @foreach ($list as $item)
-    <div class="card" style="width: 18rem;box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05); padding: 10px;margin-bottom: 15px;">
+    <div class="card" style="width: 24rem;box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05); padding: 15px;margin-bottom: 15px;">
         <div class="card-body">
-            <h5 class="card-title">{{ $item[0]->name }}</h5>
-            <p class="card-text">{{ $item[0]->observation }}</p>
-            <a href="#" class="btn btn-primary">Realizar consulta</a>
+            <h5 class="card-title">Nome: {{ $item[0]->name }}</h5>
+            <p class="card-text">Observação: {{ $item[0]->observation }}</p>
+            <p class="card-text">Entrada: 
+                <?php
+                    $data = $item[0]->entry;
+                    echo date("d/m/Y  H:i", strtotime($data));
+                ?>
+            </p>
         </div>
     </div>
 @endforeach
